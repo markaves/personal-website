7978d5753486e51626109ae7efed2349d0d8873a
Mark Neil C Aves <markaves@gmail.com>
2018-02-10 06:45:48 +0000

    Fixed typo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,15 +14,15 @@ class AboutMe extends React.Component {
 				<Grid>
                     <Row>
 					    <Col lg={12} className="text-center">
-                        <p>I am an <font color='#ff8f00'>Electronics Engineer</font> Graduate</p>
-                        <p>I was Test Engineer at <font color='#ff8f00'>Analog devices</font></p>
+                        <p>I am an <font color='#ff8f00'>Electronics Engineering</font> graduate</p>
+                        <p>I was <font color='#ff8f00'>Test Engineer</font> at <font color='#ff8f00'>Analog devices</font></p>
                         <p>I changed my career for the love of IT</p>
                         <p>I got a job as a Software Engineer at <font color='#ff8f00'>Accenture</font></p>
                         <p>I handled Automation Scripting and Middleware</p>
-                        <p>I opened my eyes on the possibilities of the Web</p>
+                        <p>It opened my eyes on the possibilities of the Web</p>
                         <p>I became a Web Engineer at <font color='#ff8f00'>Hewlett Packard</font></p>
-                        <p>I wanted more challenges so I learned Java in HP</p>
-                        <p>Now am a <font color='#ff8f00'>DevOps Engineer</font> at <font color='#ff8f00'>Macquarie</font></p>
+                        <p>I wanted more challenges so I learned Java</p>
+                        <p>Now I am a practicing<font color='#ff8f00'> DevOps</font> at <font color='#ff8f00'>Macquarie</font></p>
             			</Col>
 					</Row>
 		        </Grid>
