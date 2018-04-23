c4987b3456f3a15b51e21150c40bc528fa1e866d
Mark Neil C Aves <markaves@gmail.com>
2018-04-23 06:34:35 +0000

    Dev

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,7 @@
 import { slide as Menu } from 'react-burger-menu'
+import { Image } from 'react-bootstrap';
 import React, { Component } from 'react';
+import react from '../img/react.jpg'
 
 class SMenu extends Component {
   showSettings (event) {
@@ -14,6 +16,7 @@ class SMenu extends Component {
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        <Image src={react} height="60" />
       </Menu>
     );
   }
